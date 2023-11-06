7dcfe75af9e4649e71fbdd2e966853bb3222fb92
Gerald Bauer <gerald.bauer@gmail.com>
2023-11-06 10:39:17 +0100

    up

---
+++ programming-uniswap/README.md
@@ -0,0 +1,42 @@
+Decentralized Exchange (DEX) • Automated Market Makers (AMMs) • Liquidity Pools (LPs) • Token Swapping • Price Oracle • Constant Product Function • Infinite Reserves
+
+
+# Programming (Decentralized Finance - DeFi) - Uniswap V2 Contracts - Ruby / Rubidity Edtion
+
+
+## What's happening here?
+
+Ivan Kuznetsov  
+([web / blog](https://jeiwan.net),
+ [github](https://github.com/Jeiwan),
+ [twitter](https://twitter.com/jeiwan7)
+) has written the definite (free) in-depth (let's rebuild the contracts from scratch) articles on Uniswap [V1](https://jeiwan.net/posts/programming-defi-uniswap-1/), [V2](#the_uniswap_v2_series) & [V3](https://jeiwan.net/posts/uniswap-v3-development-book-is-out/).
+
+
+### The Uniswap V2 Series
+
+- [Programming DeFi: Uniswap V2. Part 1](https://jeiwan.net/posts/programming-defi-uniswapv2-1/),  ([source](https://github.com/Jeiwan/zuniswapv2/tree/part_1))
+- [Programming DeFi: Uniswap V2. Part 2](https://jeiwan.net/posts/programming-defi-uniswapv2-2/),  ([source](https://github.com/Jeiwan/zuniswapv2/tree/part_2))
+- [Programming DeFi: Uniswap V2. Part 3](https://jeiwan.net/posts/programming-defi-uniswapv2-3/),  ([source](https://github.com/Jeiwan/zuniswapv2/tree/part_3))
+- [Programming DeFi: Uniswap V2. Part 4](https://jeiwan.net/posts/programming-defi-uniswapv2-4/),  ([source](https://github.com/Jeiwan/zuniswapv2/tree/part_4))
+ 
+
+
+The idea is to rebuild the contracts (and tests) from scratch in Ruby / Rubidity!
+
+- [/part1](part1)
+- /part2   SOON!
+- /part3   SOON!
+- /part4   SOON!
+
+
+
+
+
+## Questions? Comments?
+
+Join us in the [Rubidity (community) discord (chat server)](https://discord.gg/3JRnDUap6y). Yes you can.
+Your questions and commentary welcome.
+
+Or post them over at the [Help & Support](https://github.com/geraldb/help) page. Thanks.
+
---
+++ programming-uniswap/part1/upstream/ZuniswapV2Pair.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+import "solmate/tokens/ERC20.sol";
+import "./libraries/Math.sol";
+
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+
+    function transfer(address to, uint256 amount) external;
+}
+
+error InsufficientLiquidityMinted();
+error InsufficientLiquidityBurned();
+error TransferFailed();
+
+contract ZuniswapV2Pair is ERC20, Math {
+    uint256 constant MINIMUM_LIQUIDITY = 1000;
+
+    address public token0;
+    address public token1;
+
+    uint112 private reserve0;
+    uint112 private reserve1;
+
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Sync(uint256 reserve0, uint256 reserve1);
+
+    constructor(address token0_, address token1_)
+        ERC20("ZuniswapV2 Pair", "ZUNIV2", 18)
+    {
+        token0 = token0_;
+        token1 = token1_;
+    }
+
+    function mint() public {
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 amount0 = balance0 - _reserve0;
+        uint256 amount1 = balance1 - _reserve1;
+
+        uint256 liquidity;
+
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = Math.min(
+                (amount0 * totalSupply) / _reserve0,
+                (amount1 * totalSupply) / _reserve1
+            );
+        }
+
+        if (liquidity <= 0) revert InsufficientLiquidityMinted();
+
+        _mint(msg.sender, liquidity);
+
+        _update(balance0, balance1);
+
+        emit Mint(msg.sender, amount0, amount1);
+    }
+
+    function burn() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 liquidity = balanceOf[msg.sender];
+
+        uint256 amount0 = (liquidity * balance0) / totalSupply;
+        uint256 amount1 = (liquidity * balance1) / totalSupply;
+
+        if (amount0 <= 0 || amount1 <= 0) revert InsufficientLiquidityBurned();
+
+        _burn(msg.sender, liquidity);
+
+        _safeTransfer(token0, msg.sender, amount0);
+        _safeTransfer(token1, msg.sender, amount1);
+
+        balance0 = IERC20(token0).balanceOf(address(this));
+        balance1 = IERC20(token1).balanceOf(address(this));
+
+        _update(balance0, balance1);
+
+        emit Burn(msg.sender, amount0, amount1);
+    }
+
+    function sync() public {
+        _update(
+            IERC20(token0).balanceOf(address(this)),
+            IERC20(token1).balanceOf(address(this))
+        );
+    }
+
+    function getReserves()
+        public
+        view
+        returns (
+            uint112,
+            uint112,
+            uint32
+        )
+    {
+        return (reserve0, reserve1, 0);
+    }
+
+    //
+    //
+    //
+    //  PRIVATE
+    //
+    //
+    //
+    function _update(uint256 balance0, uint256 balance1) private {
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+
+        emit Sync(reserve0, reserve1);
+    }
+
+    function _safeTransfer(
+        address token,
+        address to,
+        uint256 value
+    ) private {
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSignature("transfer(address,uint256)", to, value)
+        );
+        if (!success || (data.length != 0 && !abi.decode(data, (bool))))
+            revert TransferFailed();
+    }
+}
